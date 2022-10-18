54d3656696719d3bd9ab6437dcc4d7fd36528c2c
nhatquang1205 <trannhatquang2001@gmail.com>
2022-10-18 11:18:20 +0700

    Account Service, AlertService, AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { PagesComponent } from './pages/pages.component';
+import { PagesComponent } from './pages/pages.component';
+import { AuthGuard } from './_helpers/auth.guard';
 
 const routes: Routes = [
-  // {path:'', component:PagesComponent},
+  { path:'', component:PagesComponent, canActivate: [AuthGuard] },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
 ];
