c4e60211562adbe0bd87e1bd8ce34d0b96befcab
nhatquang1205 <76160177+nhatquang1205@users.noreply.github.com>
2022-10-19 16:45:04 +0700

    HC-69 [FE] Refresh CurrentUser Auth Guard
    Refresh Token Auth Guard (#5)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppSettings } from './app.settings';
 import { Settings } from './app.settings.model';
 import { AccountsService } from './_services/accounts.service';
@@ -8,10 +8,13 @@ import { AccountsService } from './_services/accounts.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'happy-care-admin';
   public settings: Settings;
   constructor(public appSettings:AppSettings, public accountsService: AccountsService){
       this.settings = this.appSettings.settings;
+  }
+  ngOnInit(): void {
+    this.accountsService.refreshToken();
   }
 }
