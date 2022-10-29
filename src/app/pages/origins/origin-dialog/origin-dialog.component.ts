5071b24e648fbfcba7295b6bf1bc3acfbd696185
admin <trannhutri0703@gmail.com>
2022-10-29 11:50:34 +0700

    add origin dialog component

---
+++ src/app/pages/origins/origin-dialog/origin-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-origin-dialog',
+  templateUrl: './origin-dialog.component.html',
+  styleUrls: ['./origin-dialog.component.scss']
+})
+export class OriginDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
