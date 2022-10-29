240f38f94fa0e560f35f811021df176dc579fd9c
admin <trannhutri0703@gmail.com>
2022-10-29 10:39:09 +0700

    add product dialog

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
 import { ButtonModule } from 'primeng/button';
 import { DialogService } from 'primeng/dynamicdialog';
+import { ProductDialogComponent } from './product-dialog/product-dialog.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 export const routes: Routes = [
   { path: '', component: ProductsComponent, pathMatch: 'full' },
@@ -10,10 +15,17 @@ export const routes: Routes = [
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forChild(routes),
-    ButtonModule
+    ButtonModule,
+    ReactiveFormsModule,
+    DialogModule,
+    InputTextModule
   ],
-  declarations: [ProductsComponent],
+  declarations: [
+    ProductDialogComponent,
+    ProductsComponent
+  ],
   providers: [
     DialogService
   ],
