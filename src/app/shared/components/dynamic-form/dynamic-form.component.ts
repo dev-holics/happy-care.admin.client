c355fe647966db9155a513a450a453d58f2af2de
admin <trannhutri0703@gmail.com>
2022-12-21 22:51:46 +0700

    fix: Update dynamic form component

--- src/app/shared/components/dynamic-form/dynamic-form.component.ts
+++ src/app/shared/components/dynamic-form/dynamic-form.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { QuestionBaseModel } from '../../models/question-base.model';
+import { QuestionControlService } from '../../services/question-control.service';
 
 @Component({
-  selector: 'app-dynamic-form',
+  selector: 'dynamic-form',
   templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.scss']
+  providers: [QuestionControlService],
 })
 export class DynamicFormComponent implements OnInit {
+  @Input() questions: QuestionBaseModel<string | string[]>[] | null = [];
+  form!: FormGroup;
+  payLoad = '';
 
-  constructor() { }
+  constructor(private qcs: QuestionControlService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = this.qcs.getFormGroup(
+      this.questions as QuestionBaseModel<string>[]
+    );
   }
 
+  onSubmit() {
+    this.payLoad = JSON.stringify(this.form.getRawValue());
+  }
 }
