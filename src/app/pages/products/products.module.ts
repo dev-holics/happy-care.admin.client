e9132da63cdeedfda88b634bf3e7ec56a7494d3b
admin <trannhutri0703@gmail.com>
2022-12-26 01:26:37 +0700

    fix(Product): Update product module

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -1,51 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './components/products/products.component';
-import { ButtonModule } from 'primeng/button';
-import { DialogService } from 'primeng/dynamicdialog';
-import { ProductDialogComponent } from './components/product-dialog/product-dialog.component';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DialogModule } from 'primeng/dialog';
-import { InputTextModule } from 'primeng/inputtext';
-import { DropdownModule } from 'primeng/dropdown';
-import { TableModule } from 'primeng/table';
-import { PaginatorModule } from 'primeng/paginator';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
-import { ToastModule } from 'primeng/toast';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-
-export const routes: Routes = [
-  { path: '', component: ProductsComponent, pathMatch: 'full' },
-];
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ProductDialogComponent } from './components/product-dialog/product-dialog.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    ButtonModule,
-    DialogModule,
-    ReactiveFormsModule,
-    InputTextModule,
-    DropdownModule,
-    TableModule,
-    PaginatorModule,
-    ToolbarModule,
-    ConfirmDialogModule,
-    MessagesModule,
-    MessageModule,
-    ToastModule,
-    InputTextareaModule
-  ],
-  declarations: [
-    ProductDialogComponent,
-    ProductsComponent
-  ],
-  providers: [
-    DialogService
-  ],
+  imports: [CommonModule, ProductsRoutingModule, SharedModule.forRoot()],
+  declarations: [ProductsComponent, ProductDialogComponent],
 })
 export class ProductsModule {}
